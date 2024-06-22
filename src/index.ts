7ae2b65d0e3bc3140f3f98719b181612a7431742
biniruu <biniruu@gmail.com>
2024-06-22 21:20:57 +0900

    feat(index.ts): implement providing json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-const rootElem = document.getElementById('app') as HTMLDivElement
-// TODO: remove the element below
-const paramElem = document.createElement('p')
-paramElem.textContent = 'Hello world'
-rootElem.appendChild(paramElem)
+import { stringify } from 'javascript-stringify'
+
+import env from '@/data/eslint/env.json'
+
+const mainElem = document.getElementById('app') as HTMLElement
+// const sidebarElem = document.getElementById('sidebar')
+
+const elem = document.createElement('textarea')
+elem.classList.add('code')
+// elem.textContent = stringify(jestTypescript, null, '\n') as string
+elem.textContent = stringify(env, null, 4) as string
+// elem.textContent = JSON.stringify(jestConfig)
+mainElem.appendChild(elem)
