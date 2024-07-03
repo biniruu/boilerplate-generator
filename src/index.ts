c5dbaebaa5f3abb15a54752954eb41e9ddf6507e
biniruu <biniruu@gmail.com>
2024-07-03 17:38:54 +0900

    refactor(index.ts): improve readability

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import generateConfig from '@generators/config'
 import type { Tab } from '_types'
 import './style.css'
 
-const tabElems = document.querySelector<HTMLDivElement>('#tabs')
-let currentTab: Tab = 'eslint'
+// Toggle 'active' css class on inputs in 'Syntax' and 'JavaScript library' categories in the sidebar
 const tabEvent = (e: MouseEvent) => {
   const tablinkElems = document.querySelectorAll<HTMLButtonElement>('.tablinks')
   tablinkElems.forEach(tablink => tablink.classList.remove('active'))
@@ -12,12 +11,64 @@ const tabEvent = (e: MouseEvent) => {
   const target = e.target as HTMLButtonElement
   target.classList.add('active')
   const value = target.value as Tab
+
   provideConfig(value)
 }
-
+let currentTab: Tab = 'eslint'
+const tabElems = document.querySelector<HTMLDivElement>('#tabs')
 tabElems && tabElems.addEventListener('click', tabEvent, { passive: true })
 
+// Handle inputs in the sidebar
+const syntax = ['typescript', 'javascript']
+const jsLib = ['nothing', 'gatsby', 'next', 'nuxt', 'react', 'vue']
+const radioBtns = [...syntax, ...jsLib]
+const formEvent = (e: MouseEvent) => {
+  const target = e.target as HTMLInputElement
+  const value = target.value as keyof typeof config
+
+  // Avoid invoking this function when user clicked outside of input
+  if (!value) {
+    return
+  }
+  if (radioBtns.includes(value)) {
+    handleRadioBtns(value)
+  } else {
+    config[value] = !config[value]
+  }
+
+  provideContents(currentTab)
+}
+const handleRadioBtns = (value: string) => {
+  // Reset inputs in 'Syntax' and 'JavaScript library' categories
+  const target = syntax.includes(value) ? syntax : jsLib
+  target.forEach(item => (config[item as keyof typeof config] = false))
+  // Select new one
+  config[value as keyof typeof config] = true
+}
+const form = document.querySelector<HTMLFormElement>('#options')
+form && form.addEventListener('click', formEvent, { passive: true })
+
+// Show code and commands to the code windows
+const provideConfig = (tab: Tab) => {
+  currentTab = tab
+  if (codeElem) {
+    codeElem.textContent = generateConfig(tab, config)
+  }
+}
+const provideCommand = () => {
+  if (bashElem) {
+    bashElem.textContent = generateCommands(config)
+  }
+}
 const codeElem = document.querySelector<HTMLElement>('#code')
+const bashElem = document.querySelector<HTMLElement>('#bash')
+
+// Init content
+const provideContents = (tab: Tab = currentTab) => {
+  provideConfig(tab)
+  provideCommand()
+}
+window.onload = () => provideContents()
 
 const config = {
   axios: false,
@@ -69,58 +120,3 @@ const config = {
   webpack: false,
   wordpress: false,
 }
-
-const bashElem = document.querySelector<HTMLElement>('#bash')
-
-const syntax = ['typescript', 'javascript']
-const jsLib = ['nothing', 'gatsby', 'next', 'nuxt', 'react', 'vue']
-const radioBtns = [...syntax, ...jsLib]
-
-// Toggling 'active' css class on inputs in 'Syntax' and 'JavaScript library' categories in the sidebar
-const handleRadioBtns = (value: string) => {
-  const target = syntax.includes(value) ? syntax : jsLib
-  target.forEach(item => (config[item as keyof typeof config] = false))
-  config[value as keyof typeof config] = true
-  provideContents(currentTab)
-}
-
-const formEvent = (e: MouseEvent) => {
-  const target = e.target as HTMLInputElement
-  const value = target.value
-
-  // Avoiding invoking this function when user clicked outside of input
-  if (!value) {
-    return
-  }
-  if (radioBtns.includes(value)) {
-    handleRadioBtns(value)
-
-    return
-  }
-
-  config[value as keyof typeof config] = !config[value as keyof typeof config]
-  provideContents(currentTab)
-}
-
-const form = document.querySelector<HTMLFormElement>('#options')
-form && form.addEventListener('click', formEvent, { passive: true })
-
-const provideConfig = (tab: Tab) => {
-  currentTab = tab
-  if (codeElem) {
-    codeElem.textContent = generateConfig(tab, config)
-  }
-}
-
-const provideCommand = () => {
-  if (bashElem) {
-    bashElem.textContent = generateCommands(config)
-  }
-}
-
-// Init content
-const provideContents = (tab: Tab = currentTab) => {
-  provideConfig(tab)
-  provideCommand()
-}
-window.onload = () => provideContents()
