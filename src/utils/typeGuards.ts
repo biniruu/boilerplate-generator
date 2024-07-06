8d0bbb95fa65ff40ba03e6de0158e9d52a1f4f0e
biniruu <biniruu@gmail.com>
2024-07-07 08:45:43 +0900

    feat: remove unnecessary type guard

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,10 +1,5 @@
-import type { Conditions, SelectOptions } from '_types'
-
 const isHtmlInputElement = (element: unknown): element is HTMLInputElement => element instanceof HTMLInputElement
 
 const isHtmlButtonElement = (element: unknown): element is HTMLButtonElement => element instanceof HTMLButtonElement
 
-const isCondition = (key: string, configOptions: SelectOptions): key is Conditions =>
-  Object.values(configOptions).includes(key)
-
-export { isCondition, isHtmlButtonElement, isHtmlInputElement }
+export { isHtmlButtonElement, isHtmlInputElement }
