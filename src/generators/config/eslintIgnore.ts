3a5a8a5314ad50025a7a9019dd7fa6b98f95562f
biniruu <biniruu@gmail.com>
2024-07-08 03:24:14 +0900

    feat: implement providing eslintignore files

---
+++ src/generators/config/eslintIgnore.ts
@@ -0,0 +1,27 @@
+import getCertainConditions from '@utils/certainConditions'
+import type { SelectOptions } from '_types'
+
+const generateEslintIgnoreConfig = (configOptions: SelectOptions) => {
+  const { hasReact, hasStorybook, hasTypescript, hasWordpress } = getCertainConditions(configOptions)
+
+  const storybook = hasStorybook && `storybook-static/`
+  const tsconfig = hasTypescript && `**/tsconfig.json`
+  const tsconfigs =
+    hasTypescript &&
+    !hasReact &&
+    !hasWordpress &&
+    `
+**/tsconfig.build.json
+**/tsconfig.default.json
+**/tsconfig.test.json`
+
+  const config = `# files
+${tsconfig || ''}${tsconfigs || ''}
+
+# paths
+node_modules/${storybook || ''}`
+
+  return config
+}
+
+export default generateEslintIgnoreConfig
