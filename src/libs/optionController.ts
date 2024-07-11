82f980ad71b2904554b77e697aefb04b2f923348
biniruu <biniruu@gmail.com>
2024-07-11 20:47:10 +0900

    style(optionController.ts): add comment

---
+++ src/libs/optionController.ts
@@ -0,0 +1,21 @@
+/**
+ * <For Nuxt.js>
+ *
+ * "disable"
+ * Immer
+ * NextAuth.js
+ * React hook form
+ * React infinite scroller
+ * React joyride
+ * Recoil
+ * SWR
+ * Tanstack query - since not yet prepared
+ * Vite
+ *
+ * <For React.js>
+ *
+ * "enable"
+ * Vite
+ *
+ *
+ */
