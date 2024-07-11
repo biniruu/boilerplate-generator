89e29af8cc043ff9320820cbf851b59e5150c037
biniruu <biniruu@gmail.com>
2024-07-12 01:20:52 +0900

    style(convertToString.ts): add comment

--- src/utils/convertToString.ts
+++ src/utils/convertToString.ts
@@ -1,5 +1,6 @@
 import { stringify } from 'javascript-stringify'
 
+// Convert a JSON string to a JavaScript string
 const convertToString = <T>(value: T, replacer?: (value: T) => string) => stringify(value, replacer, 2)
 
 export default convertToString
