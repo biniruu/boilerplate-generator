bdfbca568a20691bdd5490b667299870fc8764a8
biniruu <biniruu@gmail.com>
2024-07-12 20:10:58 +0900

    feat(conditions.ts): add isCondition

--- src/data/conditions.ts
+++ src/data/conditions.ts
@@ -1,11 +1,16 @@
 import capitaliseFirstLetter from '@utils/capitaliseFirstLetter'
+import { isCondition } from '@utils/typeGuards'
+import type { Condition } from '_types'
 
 import { options } from './options'
 
 const conditions = options.reduce((acc, curr) => {
-  const condition = 'has'.concat(capitaliseFirstLetter(curr)) as Condition
-  acc.push(condition)
-
+  const condition = 'has'.concat(capitaliseFirstLetter(curr))
+  if (isCondition(condition)) {
+    acc.push(condition)
+  } else {
+    console.error(`Condition ${condition} is not a valid condition`)
+  }
   return acc
 }, [] as Condition[])
 
