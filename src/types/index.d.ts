10cb4750f49047ccf26a3e2f8cc5ec257d273a80
biniruu <biniruu@gmail.com>
2024-07-12 19:00:06 +0900

    feat(index.d.ts): add DynamicTabValueList type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,21 +1,38 @@
 import configs from '@data/configs'
+import dynamicTabList from '@data/dynamicTabList'
 import files from '@data/files'
 import { options } from '@data/options'
 
 // See options: /src/data/options.ts
-export type Option = (typeof options)[number]
-export type SelectOptions = {
+type Option = (typeof options)[number]
+type SelectOptions = {
   [K in (typeof options)[number]]: boolean
 }
 
-export type Condition = MakeType<'has', Capitalize<Option>>
+type Condition = MakeType<'has', Capitalize<Option>>
 type MakeType<P extends string, F extends string> = `${P}${Capitalize<F>}`
 
-export type Tab = ConfigTab | FileTab | 'terminal'
+type Tab = ConfigTab | FileTab | 'terminal'
 // See files: /src/data/files.ts
-export type FileTab = (typeof files)[number]
+type FileTab = (typeof files)[number]
 // See configs: /src/data/configs.ts
-export type ConfigTab = (typeof configs)[number]
+type ConfigTab = (typeof configs)[number]
 
-export type ObjConfigTab = Record<ConfigTab, string>
-export type ObjFileTab = Record<FileTab, string>
+type ObjConfigTab = Record<ConfigTab, string>
+type ObjFileTab = Record<FileTab, string>
+
+// See dynamicTabList: /src/data/dynamicTabList.ts
+type DynamicTabValueList = keyof typeof dynamicTabList
+
+export {
+  Condition,
+  ConfigTab,
+  DynamicTabValueList,
+  FileTab,
+  MakeType,
+  ObjConfigTab,
+  ObjFileTab,
+  Option,
+  SelectOptions,
+  Tab,
+}
