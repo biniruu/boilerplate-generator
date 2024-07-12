88e2cde481cc5eff51eec8ef243162be38663156
biniruu <biniruu@gmail.com>
2024-07-13 01:57:52 +0900

    feat(typeGuards.ts): correct isCondition

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,8 +1,9 @@
-import { conditions } from '@data/conditions'
 import configs from '@data/configs'
 import files from '@data/files'
 import { options } from '@data/options'
 import type { Condition, ConfigTab, FileTab, Option, Tab } from '_types'
+
+import capitaliseFirstLetter from './capitaliseFirstLetter'
 
 const tabs = [...configs, ...files, 'terminal']
 
@@ -12,6 +13,8 @@ const isTab = (value: string): value is Tab => tabs.some(tab => tab === value)
 const isConfig = (value: string): value is ConfigTab => configs.some(config => config === value)
 const isFile = (value: string): value is FileTab => files.some(file => file === value)
 const isOption = (value: string): value is Option => options.some(option => option === value)
-const isCondition = (value: string): value is Condition => conditions.some(condition => condition === value)
+// It could be occurring an error 'ReferenceError: Cannot access uninitialized variable' when use 'conditions' via @data/conditions.ts instead of 'options'
+const isCondition = (value: string): value is Condition =>
+  options.some(option => 'has'.concat(capitaliseFirstLetter(option)) === value)
 
 export { isCondition, isConfig, isFile, isHtmlButtonElement, isHtmlInputElement, isOption, isTab }
