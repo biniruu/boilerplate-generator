a827122fb46c37279495fe8f8e2dbf8aa9f8603d
biniruu <biniruu@gmail.com>
2024-07-13 05:19:45 +0900

    style(toggleTabs.ts): convert jsdoc into comment

--- src/libs/toggleTabs.ts
+++ src/libs/toggleTabs.ts
@@ -1,116 +1,114 @@
 import dynamicTabList from '@data/dynamicTabList'
 import type { DynamicTabValueList } from '_types'
 
-/**
- * <Always showed>
- *
- * .gitignore
- * package.json
- * terminal
- *
- * <For ESLint>
- *
- * .eslintrc.js
- * .eslintignore
- *
- * <For Prettier>
- *
- * .prettierrc
- * .prettierignore
- *
- * <For Stylelint>
- *
- * .stylelintrc.js
- * .stylelintignore
- *
- * gatsby-config.ts
- *
- * <For Gatsby.js>
- *
- * gatsby-config.ts
- *
- * <For Jest>
- *
- * jest.config.ts
- * jest.setup.ts
- *
- * <For Markdownlint>
- *
- * markdownlint.json
- *
- * <For Next.js>
- *
- * next.config.ts
- *
- * [with tanstack query or swr]
- * layout.tsx
- * reactQueryProvider.tsx
- * swrProvider.tsx
- *
- * <For Nodemon>
- *
- * nodemon.json
- *
- * <For npm>
- *
- * .npmrc
- *
- * <For Nuxt.js>
- *
- * nuxt.config.ts
- * volar.config.cjs
- *
- * <For React.js>
- * @desc In this repository, a React.js is served using Vite, so it always includes a Vite configuration.
- *
- * vite.config.ts
- *
- * <For PostCSS>
- *
- * postcss.config.js
- *
- * <For Pug>
- *
- * home.pug
- * pug-lintrc.json
- *
- * [with tanstack query or swr]
- * layout.tsx
- * reactQueryProvider.tsx
- * swrProvider.tsx
- *
- * <For Socket.io>
- *
- * socket.d.ts
- *
- * <For Tailwind CSS>
- *
- * style.css
- * tailwind.config.js
- *
- * <For TypeScript>
- *
- * declarations.d.ts
- * tsconfig.json
- * tsconfig.build.json
- * tsconfig.default.json
- * tsconfig.node.json
- * tsconfig.test.json
- * typeGuards.ts
- *
- * <For Vanilla.js>
- *
- * .babelrc
- *
- * <For Vite>
- * @desc A vite configuration is for a React.js project for now.
- *
- * vite.config.ts
- *
- * <For Webpack>
- *
- * webpack.config.ts
- */
+// <Always showed>
+//
+// .gitignore
+// package.json
+// terminal
+//
+// <For ESLint>
+//
+// .eslintrc.js
+// .eslintignore
+//
+// <For Prettier>
+//
+// .prettierrc
+// .prettierignore
+//
+// <For Stylelint>
+//
+// .stylelintrc.js
+// .stylelintignore
+//
+// gatsby-config.ts
+//
+// <For Gatsby.js>
+//
+// gatsby-config.ts
+//
+// <For Jest>
+//
+// jest.config.ts
+// jest.setup.ts
+//
+// <For Markdownlint>
+//
+// markdownlint.json
+//
+// <For Next.js>
+//
+// next.config.ts
+//
+// [with tanstack query or swr]
+// layout.tsx
+// reactQueryProvider.tsx
+// swrProvider.tsx
+//
+// <For Nodemon>
+//
+// nodemon.json
+//
+// <For npm>
+//
+// .npmrc
+//
+// <For Nuxt.js>
+//
+// nuxt.config.ts
+// volar.config.cjs
+//
+// <For React.js>
+// @desc In this repository, a React.js is served using Vite, so it always includes a Vite configuration.
+//
+// vite.config.ts
+//
+// <For PostCSS>
+//
+// postcss.config.js
+//
+// <For Pug>
+//
+// home.pug
+// pug-lintrc.json
+//
+// [with tanstack query or swr]
+// layout.tsx
+// reactQueryProvider.tsx
+// swrProvider.tsx
+//
+// <For Socket.io>
+//
+// socket.d.ts
+//
+// <For Tailwind CSS>
+//
+// style.css
+// tailwind.config.js
+//
+// <For TypeScript>
+//
+// declarations.d.ts
+// tsconfig.json
+// tsconfig.build.json
+// tsconfig.default.json
+// tsconfig.node.json
+// tsconfig.test.json
+// typeGuards.ts
+//
+// <For Vanilla.js>
+//
+// .babelrc
+//
+// <For Vite>
+// A vite configuration is for a React.js project for now.
+//
+// vite.config.ts
+//
+// <For Webpack>
+//
+// webpack.config.ts
 
 const enabledTabs: DynamicTabValueList[] = []
 const lints = ['eslint', 'prettier', 'stylelint']
