729e1899650eac7e0f6c5e611fad48d89ca4cc7c
biniruu <biniruu@gmail.com>
2024-07-14 16:09:37 +0900

    test(certainConditions.test.ts): add unit test

---
+++ tests/utils/certainConditions.test.ts
@@ -0,0 +1,17 @@
+import { objOptions } from '@data/options'
+import getCertainConditions from '@utils/certainConditions'
+
+describe('Getting certain conditions', () => {
+  test('should convert received options from a parameter to conditions', () => {
+    const conditions = {
+      hasAxios: false,
+      hasNextAuth: false,
+      hasTanstackQuery: false,
+      hasTypescript: false,
+      hasTsExtension: false,
+    }
+    const result = getCertainConditions(objOptions)
+
+    expect(result).toMatchObject(conditions)
+  })
+})
