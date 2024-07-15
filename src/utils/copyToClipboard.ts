cb7afd8752244b075c5209e8ed86c9368e7d5498
biniruu <biniruu@gmail.com>
2024-07-15 13:24:18 +0900

    feat(copyToClipboard.ts): remove unnecessary type assertion

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -3,7 +3,7 @@ const copyToClipboard = async (text: string) => {
     await navigator.clipboard.writeText(text)
     alert('Code copied to clipboard')
   } catch (err) {
-    const error = err as unknown as Error
+    const error = err as Error
     console.error('Failed to copy code: ', error.message)
     alert('Something went wrong with copying code to Clipboard')
   }
