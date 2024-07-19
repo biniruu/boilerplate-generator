312fbad53c7a5c152321f5317854ca077a37e290
biniruu <biniruu@gmail.com>
2024-07-19 18:36:32 +0900

    test(bundler.test.ts): add unit test
    It should generate command for vite

---
+++ tests/generators/command/bundler.test.ts
@@ -0,0 +1,14 @@
+import getBundlerCommands from '@generators/command/bundler'
+
+import { configOptions } from './configOptions.test'
+
+describe('Generate command for Bundler', () => {
+  test('should generate command for vite', () => {
+    configOptions.vite = true
+
+    const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
+
+    expect(bundlerDependencies).toEqual(['vite'])
+    expect(bundlerDevDependencies).toEqual(['vite-plugin-dts'])
+  })
+})
