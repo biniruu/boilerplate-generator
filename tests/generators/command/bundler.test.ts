74412eef09174e7bfa7250fba88e815d4b4487fc
biniruu <biniruu@gmail.com>
2024-07-19 18:44:43 +0900

    test(bundler.test.ts): add unit test
    It should generate command for Webpack with Next.js

--- tests/generators/command/bundler.test.ts
+++ tests/generators/command/bundler.test.ts
@@ -5,33 +5,44 @@ import { configOptions } from './configOptions.test'
 describe('Generate command for Bundler', () => {
   describe('Vite', () => {
     test('should generate command for Vite', () => {
-    configOptions.vite = true
+      configOptions.vite = true
 
-    const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
+      const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
 
-    expect(bundlerDependencies).toEqual(['vite'])
-    expect(bundlerDevDependencies).toEqual(['vite-plugin-dts'])
-  })
+      expect(bundlerDependencies).toEqual(['vite'])
+      expect(bundlerDevDependencies).toEqual(['vite-plugin-dts'])
+    })
 
     test('should generate command for Vite with TypeScript', () => {
-    configOptions.vite = true
-    configOptions.typescript = true
+      configOptions.vite = true
+      configOptions.typescript = true
 
-    const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
+      const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
 
-    expect(bundlerDependencies).toEqual(['vite'])
-    expect(bundlerDevDependencies).toEqual(['vite-tsconfig-paths', 'vite-plugin-dts'])
-  })
+      expect(bundlerDependencies).toEqual(['vite'])
+      expect(bundlerDevDependencies).toEqual(['vite-tsconfig-paths', 'vite-plugin-dts'])
+    })
 
     test('should generate command for Vite with React.js and TypeScript', () => {
-    configOptions.vite = true
-    configOptions.react = true
-    configOptions.typescript = true
+      configOptions.vite = true
+      configOptions.react = true
+      configOptions.typescript = true
 
-    const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
+      const { bundlerDependencies, bundlerDevDependencies } = getBundlerCommands(configOptions)
 
-    expect(bundlerDependencies).toBeEmpty()
-    expect(bundlerDevDependencies).toEqual(['vite-tsconfig-paths', 'vite-plugin-dts'])
+      expect(bundlerDependencies).toBeEmpty()
+      expect(bundlerDevDependencies).toEqual(['vite-tsconfig-paths', 'vite-plugin-dts'])
+    })
+  })
+
+  describe('Webpack', () => {
+    test('should generate command for Webpack with Next.js', () => {
+      configOptions.webpack = true
+      configOptions.next = true
+
+      const { bundlerDevDependencies } = getBundlerCommands(configOptions)
+
+      expect(bundlerDevDependencies).toEqual(['source-map-loader'])
     })
   })
 })
