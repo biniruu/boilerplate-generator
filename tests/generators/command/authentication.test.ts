457c8c93dc9100d6a7c85eec84956b73aa53ca60
biniruu <biniruu@gmail.com>
2024-07-19 16:11:15 +0900

    test(authentication.test.ts): remove describe
    Remove redundant 'describe' method

--- tests/generators/command/authentication.test.ts
+++ tests/generators/command/authentication.test.ts
@@ -2,38 +2,36 @@ import { objOptions } from '@data/options'
 import getAuthenticationCommands from '@generators/command/authentication'
 import type { SelectOptions } from '_types'
 
-describe('Generate commands', () => {
-  describe('getAuthenticationCommands function', () => {
-    let configOptions: SelectOptions
+describe('Generate command for authentication', () => {
+  let configOptions: SelectOptions
 
-    beforeEach(() => {
-      configOptions = { ...objOptions }
-    })
+  beforeEach(() => {
+    configOptions = { ...objOptions }
+  })
 
-    test('should generate command for bcrypt.js', () => {
-      configOptions.bcrypt = true
-      configOptions.typescript = true
+  test('should generate command for bcrypt.js', () => {
+    configOptions.bcrypt = true
+    configOptions.typescript = true
 
-      const { authenticationDependencies, authenticationDevDependencies } = getAuthenticationCommands(configOptions)
+    const { authenticationDependencies, authenticationDevDependencies } = getAuthenticationCommands(configOptions)
 
-      expect(authenticationDependencies).toEqual(['bcryptjs'])
-      expect(authenticationDevDependencies).toEqual(['@types/bcryptjs'])
-    })
+    expect(authenticationDependencies).toEqual(['bcryptjs'])
+    expect(authenticationDevDependencies).toEqual(['@types/bcryptjs'])
+  })
 
-    test('should generate command for NextAuth.js', () => {
-      configOptions.next = true
-      configOptions.nextAuth = true
+  test('should generate command for NextAuth.js', () => {
+    configOptions.next = true
+    configOptions.nextAuth = true
 
-      const { authenticationDependencies } = getAuthenticationCommands(configOptions)
+    const { authenticationDependencies } = getAuthenticationCommands(configOptions)
 
-      expect(authenticationDependencies).toEqual(['next-auth'])
-    })
+    expect(authenticationDependencies).toEqual(['next-auth'])
+  })
 
-    test('should generate an empty array when all options in configOptions are false', () => {
-      const { authenticationDependencies, authenticationDevDependencies } = getAuthenticationCommands(configOptions)
+  test('should generate an empty array when all options in configOptions are false', () => {
+    const { authenticationDependencies, authenticationDevDependencies } = getAuthenticationCommands(configOptions)
 
-      expect(authenticationDependencies).toBeEmpty()
-      expect(authenticationDevDependencies).toBeEmpty()
-    })
+    expect(authenticationDependencies).toBeEmpty()
+    expect(authenticationDevDependencies).toBeEmpty()
   })
 })
