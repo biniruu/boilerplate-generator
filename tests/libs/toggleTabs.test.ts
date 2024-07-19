eb727904886be7bea48aa1a214a61379fe6c2546
biniruu <biniruu@gmail.com>
2024-07-19 13:55:19 +0900

    test(toggleTabs.test.ts): add unit test

---
+++ tests/libs/toggleTabs.test.ts
@@ -0,0 +1,23 @@
+import dynamicTabList from '@data/dynamicTabList'
+import { createTab } from '@libs/toggleTabs'
+
+describe('Toggle tabs', () => {
+  test('should create a tab element using a template element with given data through the parameter', async () => {
+    // Insert elements into the DOM for testing
+    document.body.innerHTML = `
+      <template id="tab">
+        <button class="tablinks" type="button"></button>
+      </template>
+      <div id="dynamic-tabs"></div>`
+
+    const tab = 'eslint'
+    await createTab(tab)
+
+    const dynamicTabsElem = document.querySelector<HTMLDivElement>('#dynamic-tabs')
+    const createdTab = dynamicTabsElem && dynamicTabsElem.querySelector<HTMLButtonElement>('#eslint-tab')
+    expect(createdTab).not.toBeNull()
+    expect(createdTab && createdTab.textContent).toBe(dynamicTabList[tab])
+    expect(createdTab && createdTab.id).toBe(`${tab}-tab`)
+    expect(createdTab && createdTab.value).toBe(tab)
+  })
+})
