2764d704d0af480a2bae18bcfa951398159f01b8
biniruu <biniruu@gmail.com>
2024-07-20 14:51:20 +0900

    test(compiler.test.ts): add unit test
    It generate command for Babel with Wordpress

--- tests/generators/command/compiler.test.ts
+++ tests/generators/command/compiler.test.ts
@@ -3,10 +3,34 @@ import getCompilerCommands from '@generators/command/compiler'
 import { configOptions } from './configOptions.test'
 
 describe('Compiler', () => {
+  const devDependenciesWithoutWordpress = [
+    '@babel/node',
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-runtime',
+    '@babel/preset-env',
+  ]
+
   test('should return empty arrays when all options are false', () => {
     const { compilerDependencies, compilerDevDependencies } = getCompilerCommands(configOptions)
 
     expect(compilerDependencies).toBeEmpty()
     expect(compilerDevDependencies).toBeEmpty()
+  })
+
+  test('should generate command for Babel', () => {
+    configOptions.babel = true
+
+    const { compilerDevDependencies } = getCompilerCommands(configOptions)
+
+    expect(compilerDevDependencies).toIncludeSameMembers(['@babel/core', ...devDependenciesWithoutWordpress])
+  })
+
+  test('should generate command for Babel with Wordpress', () => {
+    configOptions.babel = true
+    configOptions.wordpress = true
+
+    const { compilerDevDependencies } = getCompilerCommands(configOptions)
+
+    expect(compilerDevDependencies).toIncludeSameMembers(['@babel/core'])
   })
 })
