08d43efd9799c62afe033e08dda3bf0f77467ea4
biniruu <biniruu@gmail.com>
2024-07-20 15:14:02 +0900

    test(configOptions.test.ts): add setHasJsLibs function

--- tests/generators/command/configOptions.test.ts
+++ tests/generators/command/configOptions.test.ts
@@ -2,6 +2,12 @@ import { objOptions } from '@data/options'
 import type { SelectOptions } from '_types'
 
 export let configOptions: SelectOptions
+export const setHasJsLibs = () => {
+  configOptions.next = true
+  configOptions.nuxt = true
+  configOptions.react = true
+  configOptions.vue = true
+}
 
 beforeEach(() => {
   configOptions = { ...objOptions }
