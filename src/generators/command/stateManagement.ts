4bb4f8054d397de14af2bb0fd9573dbb20df3caa
biniruu <biniruu@gmail.com>
2024-07-24 16:11:02 +0900

    style(stateManagement.ts): use getCertainConditions to get variable

--- src/generators/command/stateManagement.ts
+++ src/generators/command/stateManagement.ts
@@ -1,7 +1,8 @@
+import getCertainConditions from '@utils/certainConditions'
 import type { SelectOptions } from '_types'
 
 const getStateManagementCommands = (configOptions: SelectOptions) => {
-  const hasRecoil = configOptions.recoil
+  const { hasRecoil } = getCertainConditions(configOptions)
 
   const stateManagementDependencies: string[] = []
 
