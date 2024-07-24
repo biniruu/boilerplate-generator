ad4b9a1727850c0ebcb8ee39de7852d3ddd6bed5
biniruu <biniruu@gmail.com>
2024-07-24 16:53:28 +0900

    test(test.test.ts): add unit test
    It should generate a command for Jest with React.js

--- tests/generators/command/test.test.ts
+++ tests/generators/command/test.test.ts
@@ -54,4 +54,19 @@ describe('Test commands', () => {
       ...dependencies,
     ])
   })
+
+  test('should generate a command for Jest with React.js', () => {
+    configOptions.jest = true
+    configOptions.react = true
+
+    const { testDevDependencies } = getTestCommands(configOptions)
+
+    expect(testDevDependencies).toIncludeSameMembers([
+      '@testing-library/react',
+      '@testing-library/react-hooks',
+      '@types/react-test-renderer',
+      'react-test-renderer',
+      ...dependencies,
+    ])
+  })
 })
