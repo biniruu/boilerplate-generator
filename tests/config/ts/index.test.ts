e8f97598d1233b6e8e0b4a216ce31284ccc7a979
biniruu <biniruu@gmail.com>
2024-08-02 01:00:17 +0900

    test(ts/index.test.ts): add unit tests
    Generate tsconfigs

---
+++ tests/config/ts/index.test.ts
@@ -0,0 +1,45 @@
+import getTsBuild from '@generators/config/ts/tsBuild'
+import getTsConfig from '@generators/config/ts/tsConfig'
+import getTsDefault from '@generators/config/ts/tsDefault'
+import getTsNode from '@generators/config/ts/tsNode'
+import getTsTest from '@generators/config/ts/tsTest'
+
+import { elemCode, options, setTextContent } from '../setTextContent.test'
+
+test('should return tsconfig.build.json', () => {
+  setTextContent({ generateConfig: getTsBuild })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return tsconfig.test.json', () => {
+  setTextContent({ generateConfig: getTsTest })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return tsconfig.default.json', () => {
+  setTextContent({ generateConfig: getTsDefault })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return tsconfig.json', () => {
+  setTextContent({ generateConfig: getTsDefault })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return tsconfig.json when React.js is selected', () => {
+  options.react = true
+
+  setTextContent({ generateConfig: getTsConfig })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return tsconfig.node.json', () => {
+  setTextContent({ content: getTsNode() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
