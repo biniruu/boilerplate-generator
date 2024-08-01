6de798ff1ac50f10bc834b3af600edb58b813abd
biniruu <biniruu@gmail.com>
2024-08-02 01:55:21 +0900

    test(eslintignore.test.ts): add unit tests
    Generate .eslintignore files

---
+++ tests/config/eslintignore.test.ts
@@ -0,0 +1,43 @@
+import generateEslintIgnoreConfig from '@generators/config/eslintIgnore'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return .eslintignore', () => {
+  setTextContent({ content: generateEslintIgnoreConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return an .eslintignore configuration for Storybook', () => {
+  options.storybook = true
+
+  setTextContent({ content: generateEslintIgnoreConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return an .eslintignore configuration for TypeScript', () => {
+  options.typescript = true
+
+  setTextContent({ content: generateEslintIgnoreConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return an .eslintignore configuration for Wordpress', () => {
+  options.wordpress = true
+
+  setTextContent({ content: generateEslintIgnoreConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return an .eslintignore configuration for all options', () => {
+  options.typescript = true
+  options.storybook = true
+  options.wordpress = true
+
+  setTextContent({ content: generateEslintIgnoreConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
