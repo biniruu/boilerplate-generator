fcd631fe9f589da6d564f6e6348e77e5a4c6f554
biniruu <biniruu@gmail.com>
2024-08-02 13:03:41 +0900

    test(npm.test.ts): add unit test
    Test should return a configuration for npm

---
+++ tests/config/npm.test.ts
@@ -0,0 +1,9 @@
+import generateNpmConfig from '@generators/config/npm'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for npm', () => {
+  setTextContent({ content: generateNpmConfig() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
