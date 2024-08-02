16992cddfe30e08b63692e56ab9d8bdff7a40ca0
biniruu <biniruu@gmail.com>
2024-08-02 12:58:40 +0900

    test(next.test.ts): add unit tests
    Generate configurations for Next.js

---
+++ tests/config/next.test.ts
@@ -0,0 +1,17 @@
+import generateNextConfig from '@generators/config/next'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return a Next.js configuration', () => {
+  setTextContent({ content: generateNextConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a Next.js with TypeScript configuration', () => {
+  options.typescript = true
+
+  setTextContent({ content: generateNextConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
