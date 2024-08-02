b59035e0b5196952b8768db95be1ffceed43dae2
biniruu <biniruu@gmail.com>
2024-08-02 13:22:12 +0900

    test(prettier.test.ts): add unit tests
    Generate configurations for Prettier

---
+++ tests/config/prettier.test.ts
@@ -0,0 +1,34 @@
+import generatePrettierConfig from '@generators/config/prettier'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for Prettier', () => {
+  setTextContent({ content: generatePrettierConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a configuration for Prettier with Tailwind CSS', () => {
+  options.tailwind = true
+
+  setTextContent({ content: generatePrettierConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a configuration for Prettier with Vue.js', () => {
+  options.vue = true
+
+  setTextContent({ content: generatePrettierConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a configuration for Prettier with Tailwind CSS and Vue.js', () => {
+  options.tailwind = true
+  options.vue = true
+
+  setTextContent({ content: generatePrettierConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
