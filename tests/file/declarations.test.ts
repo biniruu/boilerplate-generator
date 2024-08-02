d7de52c56a9501ca7ad9760e9489f08f58706e36
biniruu <biniruu@gmail.com>
2024-08-02 20:40:03 +0900

    test(declarations.test.ts): add unit test
    Test should return a declarations.d.ts file

---
+++ tests/file/declarations.test.ts
@@ -0,0 +1,9 @@
+import getDeclarationsFile from '@generators/file/declarations'
+
+import { elemCode, setTextContent } from '../setTextContent.test'
+
+test('should return a declarations.d.ts file', () => {
+  setTextContent({ content: getDeclarationsFile() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
