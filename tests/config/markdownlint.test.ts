fb7235ffc64ff63b34f155f593e09c0e822e8128
biniruu <biniruu@gmail.com>
2024-08-02 11:16:49 +0900

    test(markdownlint.test.ts): add unit test
    Test should return a markdownlint file

---
+++ tests/config/markdownlint.test.ts
@@ -0,0 +1,9 @@
+import generateMarkdownlintConfig from '@generators/config/markdownlint'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a markdownlint file', () => {
+  setTextContent({ generateConfig: generateMarkdownlintConfig })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
