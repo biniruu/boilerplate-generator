664fbc671ca2f9d6c3e4a53a8268c60415f78872
biniruu <biniruu@gmail.com>
2024-08-02 21:05:48 +0900

    test(tailwind.test.ts): add unit test
    Test should return a tailwind.config.ts file

---
+++ tests/file/tailwind.test.ts
@@ -0,0 +1,9 @@
+import getTailwindFile from '@file/tailwind'
+
+import { elemCode, setTextContent } from '../setTextContent.test'
+
+test('should return a tailwind.config.ts', () => {
+  setTextContent({ content: getTailwindFile() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
