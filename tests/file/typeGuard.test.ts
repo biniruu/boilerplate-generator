8d87b34fd6d40baf748abcb8d64dfa3b430812b5
biniruu <biniruu@gmail.com>
2024-08-02 21:13:53 +0900

    test(typeGuard.test.ts): add unit test
    Test should return a typeGuards.ts file

---
+++ tests/file/typeGuard.test.ts
@@ -0,0 +1,9 @@
+import getTypeGuardFile from '@file/typeGuard'
+
+import { elemCode, setTextContent } from '../setTextContent.test'
+
+test('should return a typeGuards.ts file', () => {
+  setTextContent({ content: getTypeGuardFile() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
