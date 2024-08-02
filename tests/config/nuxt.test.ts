84288c4eeb9909c6397797aca13bb4725dbfdd78
biniruu <biniruu@gmail.com>
2024-08-02 13:06:06 +0900

    test(nuxt.test.ts): add unit test
    Test should return a configuration for Nuxt.js

---
+++ tests/config/nuxt.test.ts
@@ -0,0 +1,9 @@
+import generateNuxtConfig from '@generators/config/nuxt'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for Nuxt.js', () => {
+  setTextContent({ content: generateNuxtConfig() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
