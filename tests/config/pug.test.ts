8b5600829f7cc8d5a1cce98f2cd38ea7067426f9
biniruu <biniruu@gmail.com>
2024-08-02 13:26:13 +0900

    test(pug.test.ts): add unit test
    Test should return a configuration for Pug

---
+++ tests/config/pug.test.ts
@@ -0,0 +1,9 @@
+import generatePugConfig from '@generators/config/pug'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for Pug', () => {
+  setTextContent({ content: generatePugConfig() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
