321d17ff4fe1ee8b0fbf6ac58f7424707b7179f0
biniruu <biniruu@gmail.com>
2024-08-02 20:59:28 +0900

    test(reactQueryProvider.test.ts): add unit tests
    Generate files for reactQueryProvider.tsx

---
+++ tests/file/reactQueryProvider.test.ts
@@ -0,0 +1,17 @@
+import getReactQueryProviderFile from '@file/reactQueryProvider'
+
+import { elemCode, options, setTextContent } from '../setTextContent.test'
+
+test('should return a reactQueryProvider.tsx file', () => {
+  setTextContent({ content: getReactQueryProviderFile(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a reactQueryProvider.tsx file including Next.js', () => {
+  options.next = true
+
+  setTextContent({ content: getReactQueryProviderFile(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
