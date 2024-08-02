704422828c213e6a5886b037a0bc17265c89f65c
biniruu <biniruu@gmail.com>
2024-08-02 14:03:14 +0900

    test(tailwind.test.ts): add unit test
    Test should return a configuration for Tailwind CSS

---
+++ tests/config/tailwind.test.ts
@@ -0,0 +1,17 @@
+import generateTailwindConfig from '@generators/config/tailwind'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for Tailwind CSS', () => {
+  setTextContent({ content: generateTailwindConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a configuration for Tailwind CSS with TypeScript', () => {
+  options.typescript = true
+
+  setTextContent({ content: generateTailwindConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
