7a1335e18f569e76ea9607fd4a93ff052462a0f0
biniruu <biniruu@gmail.com>
2024-08-02 13:00:13 +0900

    test(nodemon.test.ts): add unit tests
    Generate configurations for nodemon

---
+++ tests/config/nodemon.test.ts
@@ -0,0 +1,17 @@
+import generateNodemonConfig from '@generators/config/nodemon'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return a nodemon configuration', () => {
+  setTextContent({ content: generateNodemonConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a nodemon configuration for TypeScript', () => {
+  options.typescript = true
+
+  setTextContent({ content: generateNodemonConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
