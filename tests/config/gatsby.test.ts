cbe2c2cd01fe85e83c07e3be975f30fff40c49ce
biniruu <biniruu@gmail.com>
2024-08-02 10:49:55 +0900

    test(gatsby.test.ts): add unit test
    It should return a gatsby configuration

---
+++ tests/config/gatsby.test.ts
@@ -0,0 +1,9 @@
+import generateGatsbyConfig from '@generators/config/gatsby'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a gatsby configuration', () => {
+  setTextContent({ content: generateGatsbyConfig() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
