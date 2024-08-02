5e8aa12fbc7870de3d80be63292c57c55d4c0192
biniruu <biniruu@gmail.com>
2024-08-02 13:16:16 +0900

    test(postcss.test.ts): add unit tests
    Generate configurations for PostCSS

---
+++ tests/config/postcss.test.ts
@@ -0,0 +1,17 @@
+import generatePostcssConfig from '@generators/config/postcss'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for PostCSS', () => {
+  setTextContent({ content: generatePostcssConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
+
+test('should return a configuration for PostCSS with Tailwind CSS', () => {
+  options.tailwind = true
+
+  setTextContent({ content: generatePostcssConfig(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
