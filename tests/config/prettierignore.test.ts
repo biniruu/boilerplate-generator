cf19b90f5b0e39bd312cdea31e0ad7165ed8316f
biniruu <biniruu@gmail.com>
2024-08-02 13:24:13 +0900

    test(prettierignore.test.ts): add unit test
    Test should return a .prettierignore file

---
+++ tests/config/prettierignore.test.ts
@@ -0,0 +1,9 @@
+import generatePrettierIgnoreConfig from '@generators/config/prettierignore'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a .prettierignore file', () => {
+  setTextContent({ content: generatePrettierIgnoreConfig() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
