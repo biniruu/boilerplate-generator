a34a2ddbda2ec82a3f2b6fad0bfe33aba7d42fda
biniruu <biniruu@gmail.com>
2024-08-02 14:10:54 +0900

    test(volar.test.ts): add unit test
    Test should return a configuration for Volar

---
+++ tests/config/volar.test.ts
@@ -0,0 +1,9 @@
+import generateVolarConfig from '@generators/config/volar'
+
+import { elemCode, setTextContent } from './setTextContent.test'
+
+test('should return a configuration for Volar', () => {
+  setTextContent({ content: generateVolarConfig() })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
