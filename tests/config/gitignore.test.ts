ba044493e5693190bbf82dc9196b1a85f45f34b9
biniruu <biniruu@gmail.com>
2024-08-02 11:00:45 +0900

    test(gitignore.test.ts): add unit test
    It should return a link to download a .gitignore file that includes configurations for Next.js and Storybook

---
+++ tests/config/gitignore.test.ts
@@ -0,0 +1,12 @@
+import generateGitIgnore from '@generators/config/gitignore'
+
+import { elemCode, options, setTextContent } from './setTextContent.test'
+
+test('should return a link to download a .gitignore file that includes configurations for Next.js and Storybook', () => {
+  options.next = true
+  options.storybook = true
+
+  setTextContent({ content: generateGitIgnore(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
