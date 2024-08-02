436a0b87bdd5835a21409588e401e34818139cce
biniruu <biniruu@gmail.com>
2024-08-02 21:03:30 +0900

    test(swrProvider.test.ts): add unit test
    Test should return a swrProvider.tsx file

---
+++ tests/file/swrProvider.test.ts
@@ -0,0 +1,9 @@
+import getSwrProviderFile from '@file/swrProvider'
+
+import { elemCode, options, setTextContent } from '../setTextContent.test'
+
+test('should return a swrProvider.tsx file', () => {
+  setTextContent({ content: getSwrProviderFile(options) })
+
+  expect(elemCode?.textContent).toMatchSnapshot()
+})
