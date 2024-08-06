d305eb2b9b31ab1f3affa1fb92c9fc85a62363d8
biniruu <biniruu@gmail.com>
2024-08-07 04:21:41 +0900

    test(capitaliseFirstLetter.ts): remove unit test

--- tests/utils/capitaliseFirstLetter.test.ts
+++
@@ -1,7 +0,0 @@
-import capitaliseFirstLetter from '@utils/capitaliseFirstLetter'
-
-test('should make a first letter capital in a word', () => {
-  const result = capitaliseFirstLetter('axios')
-
-  expect(result).toBe('Axios')
-})
