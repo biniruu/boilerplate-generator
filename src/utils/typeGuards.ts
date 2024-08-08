a05ade92f9dba7335ecdb52a4287920e4464d3d5
biniruu <biniruu@gmail.com>
2024-08-09 02:56:39 +0900

    style(typeGuards.ts): break lines between functions

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -8,14 +8,21 @@ import { upperFirst } from 'lodash-es'
 const tabs = [...configs, ...files, 'terminal', 'readme']
 
 export const isHtmlInputElement = (element: unknown): element is HTMLInputElement => element instanceof HTMLInputElement
+
 export const isHtmlButtonElement = (element: unknown): element is HTMLButtonElement =>
   element instanceof HTMLButtonElement
+
 export const isTab = (value: string): value is Tab => tabs.some(tab => tab === value)
+
 export const isConfig = (value: string): value is ConfigTab => configs.some(config => config === value)
+
 export const isFile = (value: string): value is FileTab => files.some(file => file === value)
+
 export const isOption = (value: string): value is Option => options.some(option => option === value)
+
 // It could be occurring an error 'ReferenceError: Cannot access uninitialized variable' when use 'conditions' via @data/conditions.ts instead of 'options'
 export const isCondition = (value: string): value is Condition =>
   options.some(option => 'has'.concat(upperFirst(option)) === value)
+
 export const isDynamicTabValue = (value: string): value is DynamicTabValueList =>
   Object.keys(dynamicTabList).some(tabValue => tabValue === value)
