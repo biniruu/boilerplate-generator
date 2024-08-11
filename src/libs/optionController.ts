dfb80f78d37d4137f289acc3bdf840e04b0663c4
biniruu <biniruu@gmail.com>
2024-08-12 04:42:20 +0900

    refactor(optionController.ts): reduce functions and variables
    Move some functions and variables to optionState.ts.
    Rebuild some conditions

--- src/libs/optionController.ts
+++ src/libs/optionController.ts
@@ -20,32 +20,30 @@
  *
  */
 
+import { jsLib, radioBtns, syntax } from '@data/options'
 import { isDynamicTabValue, isOption } from '@utils/typeGuards'
-import type { DynamicTabValueList, Option, SelectOptions } from '_types'
+import type { DynamicTabValueList, JsLib, Option, SelectOptions } from '_types'
 
 import { reloadEditor, showReadme } from './editorController'
 import toggleOptionState from './optionState'
 import { getActivatedTab } from './tabController'
 import toggleTabs from './toggleTabs'
 
-type JsLib = (typeof jsLib)[number]
-
-const syntax: Option[] = ['typescript', 'javascript']
-const jsLib = ['nothing', 'gatsby', 'next', 'nuxt', 'react', 'vue', 'wordpress'] as const
-const radioBtns = [...syntax, ...jsLib]
 let precedentValue: JsLib
 
+const isJsLib = (value: string): value is JsLib => jsLib.some(item => item === value)
 export const handleOptions = (value: Option, isChecked: boolean, options: SelectOptions) => {
   if (isOption(value)) {
     // (options[value] = !options[value]) means that togging checkbox
-    radioBtns.includes(value) ? handleRadioBtns(value, options) : (options[value] = !options[value])
+    radioBtns.includes(value as (typeof radioBtns)[number])
+      ? handleRadioBtns(value, options)
+      : (options[value] = !options[value])
     reloadEditor(options)
-    toggleOptionState(value)
   }
-
-  if (jsLib.includes(value as JsLib)) {
+  if (isJsLib(value)) {
     toggleTabs(precedentValue as Extract<DynamicTabValueList, JsLib>, false)
-    precedentValue = value as JsLib
+    precedentValue = value
+    toggleOptionState(value)
   }
   if (isDynamicTabValue(value)) {
     toggleTabs(value, isChecked)
@@ -57,19 +55,10 @@ export const handleOptions = (value: Option, isChecked: boolean, options: Select
   }
 }
 
-const handleRadioBtns = (value: Option, options: SelectOptions) => {
+const handleRadioBtns = (value: string, options: SelectOptions) => {
   // Reset inputs in 'Syntax' and 'JavaScript library' categories
-  const target = syntax.includes(value) ? syntax : jsLib
+  const target = syntax.includes(value as (typeof syntax)[number]) ? syntax : jsLib
   target.forEach(item => (options[item] = false))
   // Select new one
-  options[value] = true
-}
-
-export const toggleChecked = (value: Option) => document.querySelector<HTMLInputElement>(`#${value}`)?.click()
-
-export const toggleDisabled = (value: Option) => {
-  const elem = document.querySelector<HTMLInputElement>(`#${value}`)
-  if (elem) {
-    elem.disabled = !elem.disabled
-  }
+  options[value as Option] = true
 }
