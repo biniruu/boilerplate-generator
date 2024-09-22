ed54db5552fac6bfdf6e032ebbbb5503b81813ad
biniruu <biniruu@gmail.com>
2024-09-22 19:10:38 +0900

    feat(command): add typescript

--- src/generators/command/index.ts
+++ src/generators/command/index.ts
@@ -14,6 +14,7 @@ import getPrettierCommands from './prettier'
 import getStateManagementCommands from './stateManagement'
 import getStylelintCommands from './stylelint'
 import getTestCommands from './test'
+import getTypescriptCommands from './typescript'
 import getUtilityCommands from './utility'
 import getWebFrameworkCommands from './webFramework'
 
@@ -39,6 +40,7 @@ const generateCommand = (options: SelectOptions) => {
   const { testDevDependencies } = getTestCommands(options)
   const { utilityDependencies, utilityDevDependencies } = getUtilityCommands(options)
   const { webFrameworkDependencies, webFrameworkDevDependencies } = getWebFrameworkCommands(options)
+  const { typescriptDevDependencies } = getTypescriptCommands(options)
 
   const dependencies = [
     ...authenticationDependencies,
@@ -65,6 +67,7 @@ const generateCommand = (options: SelectOptions) => {
     ...testDevDependencies,
     ...utilityDevDependencies,
     ...webFrameworkDevDependencies,
+    ...typescriptDevDependencies,
   ]
   const hasDependencies = dependencies.length
   const hasDevDependencies = devDependencies.length
---
+++ src/generators/command/typescript.ts
@@ -0,0 +1,17 @@
+import getCertainConditions from '@utils/certainConditions'
+import type { SelectOptions } from '_types'
+
+const getTypescriptCommands = (configOptions: SelectOptions) => {
+  const { hasTypescript, hasJsLibs, hasGatsby, hasWordpress } = getCertainConditions(configOptions)
+
+  const typescriptDevDependencies: string[] = []
+  const libsContainTs = hasJsLibs || hasGatsby || hasWordpress
+
+  if (hasTypescript) {
+    !libsContainTs && typescriptDevDependencies.push('typescript')
+  }
+
+  return { typescriptDevDependencies }
+}
+
+export default getTypescriptCommands
