f04b14350563766c409820e6b7a4d04532eb751d
biniruu <biniruu@gmail.com>
2024-09-25 17:29:10 +0900

    feat(webpack config): remove rule
    Remove HTML rule from generator/config/webpack/rules.ts

--- src/generators/config/webpack/rules.ts
+++ src/generators/config/webpack/rules.ts
@@ -17,10 +17,10 @@ const getRules = (configOptions: SelectOptions) => {
 
   const postcssLoader = hasPostcss ? ['postcss-loader'] : [];
   const rules: Rule[] = [
-    {
-      test: /\.html$/i,
-      type: 'asset/resource',
-    },
+    // {
+    //   test: /\.html$/i,
+    //   type: 'asset/resource',
+    // },
     {
       test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
       type: 'asset/resource',
