f5ebeda31f1730407f0282edd230a03035cbc2ec
biniruu <biniruu@gmail.com>
2024-09-27 00:51:46 +0900

    feat(dynamicTabList.ts): add details
    Properties in the dynamicTabList variable has sub-properties like value, ext and more

--- src/data/dynamicTabList.ts
+++ src/data/dynamicTabList.ts
@@ -1,39 +1,129 @@
 const dynamicTabList = {
-  eslint: '.eslintrc.js',
-  eslintFlatConfig: 'eslint.config.js',
-  prettier: '.prettierrc.js',
-  stylelint: '.stylelintrc.js',
-  jest: 'jest.config.ts',
-  postcss: 'postcss.config.js',
-  webpack: 'webpack.config.ts',
-  vite: 'vite.config.ts',
-  next: 'next.config.js',
-  tailwind: 'tailwind.config.js',
-  typescript: 'tsconfig.json',
-  babel: '.babelrc',
-  pug: '.pug-lintrc.json',
-  nodemon: 'nodemon.json',
-  markdownlint: '.markdownlint.json',
-  gatsby: 'gatsby-config.ts',
-  npm: '.npmrc',
-  nuxt: 'nuxt.config.ts',
-  volar: 'volar.config.cjs',
-  'next-layout-file': 'layout.tsx',
-  'tanstack-query-file': 'reactQueryProvider.tsx',
-  'type-guards-file': 'typeGuards.ts',
-  'jest-setup': 'jest.setup.ts',
-  'ts-default': 'tsconfig.default.json',
-  'ts-build': 'tsconfig.build.json',
-  'ts-node': 'tsconfig.node.json',
-  'ts-test': 'tsconfig.test.json',
-  'eslint-ignore': '.eslintignore',
-  'pug-file': 'home.pug',
-  'swr-file': 'swrProvider.tsx',
-  'tailwind-file': 'style.css',
-  'typescript-file': 'declarations.d.ts',
-  'socket-file': 'socket.d.ts',
-  'stylelint-ignore': '.stylelintignore',
-  'prettier-ignore': '.prettierignore',
+  eslint: {
+    value: '.eslintrc.js',
+  },
+  eslintFlatConfig: {
+    value: 'eslint.config.mjs',
+  },
+  prettier: {
+    value: '.prettierrc.js',
+  },
+  stylelint: {
+    value: '.stylelintrc.js',
+  },
+  jest: {
+    value: 'jest.config',
+    ext: {
+      javascript: '.js',
+      typescript: '.ts',
+    },
+  },
+  postcss: {
+    value: 'postcss.config.js',
+  },
+  webpack: {
+    value: 'webpack.config',
+    ext: {
+      javascript: '.js',
+      typescript: '.ts',
+    },
+  },
+  vite: {
+    value: 'vite.config.ts',
+  },
+  next: {
+    value: 'next.config.mjs',
+  },
+  tailwind: {
+    value: 'tailwind.config',
+    ext: {
+      javascript: '.js',
+      typescript: '.ts',
+    },
+  },
+  typescript: {
+    value: 'tsconfig.json',
+  },
+  babel: {
+    value: '.babelrc',
+  },
+  pug: {
+    value: '.pug-lintrc.json',
+  },
+  nodemon: {
+    value: 'nodemon.json',
+  },
+  markdownlint: {
+    value: '.markdownlint.json',
+  },
+  gatsby: {
+    value: 'gatsby-config.ts',
+  },
+  npm: {
+    value: '.npmrc',
+  },
+  nuxt: {
+    value: 'nuxt.config.ts',
+  },
+  volar: {
+    value: 'volar.config.cjs',
+  },
+  'next-layout-file': {
+    value: 'layout.tsx',
+  },
+  'tanstack-query-file': {
+    value: 'reactQueryProvider',
+    ext: {
+      javascript: '.jsx',
+      typescript: '.tsx',
+    },
+  },
+  'type-guards-file': {
+    value: 'typeGuards.ts',
+  },
+  'jest-setup': {
+    value: 'jest.setup',
+    ext: {
+      javascript: '.js',
+      typescript: '.ts',
+    },
+  },
+  'ts-default': {
+    value: 'tsconfig.default.json',
+  },
+  'ts-build': {
+    value: 'tsconfig.build.json',
+  },
+  'ts-node': {
+    value: 'tsconfig.node.json',
+  },
+  'ts-test': {
+    value: 'tsconfig.test.json',
+  },
+  'eslint-ignore': {
+    value: '.eslintignore',
+  },
+  'pug-file': {
+    value: 'home.pug',
+  },
+  'swr-file': {
+    value: 'swrProvider.tsx',
+  },
+  'tailwind-file': {
+    value: 'style.css',
+  },
+  'typescript-file': {
+    value: 'declarations.d.ts',
+  },
+  'socket-file': {
+    value: 'socket.d.ts',
+  },
+  'stylelint-ignore': {
+    value: '.stylelintignore',
+  },
+  'prettier-ignore': {
+    value: '.prettierignore',
+  },
 };
 
 export default dynamicTabList;
